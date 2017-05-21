8f0ad92ab355400d595aa3e55c535d46003f499e
Simon Shi <simonsmh@gmail.com>
2017-05-21 22:25:03 +0800

    init

--- config.sh
+++ config.sh
@@ -27,7 +27,7 @@
 
 # This will be the folder name under /magisk
 # This should also be the same as the id in your module.prop to prevent confusion
-MODID=template
+MODID=magisk-notocoloremoji-replacer
 
 # Set to true if you need to enable Magic Mount
 # Most mods would like it to be enabled
@@ -49,9 +49,9 @@ LATESTARTSERVICE=false
 # Set what you want to show when installing your mod
 
 print_modname() {
-  ui_print "*******************************"
-  ui_print "     Magisk Module Template    "
-  ui_print "*******************************"
+  ui_print "********************************"
+  ui_print " Magisk NotoColorEmoji replacer "
+  ui_print "********************************"
 }
 
 ##########################################################################################
