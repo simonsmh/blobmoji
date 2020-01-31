96f5be025146fcaa96027fc9169f7dfa4bb436c1
Maxr1998 <max.rumpf1998@gmail.com>
2020-01-31 14:44:47 +0100

    Update to latest module structure

---
+++ customize.sh
@@ -0,0 +1,12 @@
+ui_print "- Searching in fonts.xml"
+[[ -d /sbin/.magisk/mirror ]] && MIRRORPATH=/sbin/.magisk/mirror || unset MIRRORPATH
+FILE=/system/etc/fonts.xml
+FILENAME=$(sed -ne '/<family lang="und-Zsye".*>/,/<\/family>/ {s/.*<font weight="400" style="normal">\(.*\)<\/font>.*/\1/p;}' $MIRRORPATH$FILE)
+for i in $FILENAME
+do
+    ui_print "- Copying fonts files to $i"
+    cp -f $MODPATH/system/fonts/Blobmoji.ttf $MODPATH/system/fonts/$i
+done
+
+ui_print "- Setting permissions"
+set_perm_recursive $MODPATH 0 0 0755 0644
